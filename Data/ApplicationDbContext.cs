90c470d24f79bd987c950c9b924ab54b10cd3361
FrJ03 <franjomor@gmail.com>
2024-12-29 18:55:49 +0100

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
